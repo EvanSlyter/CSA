a4c401864705caf02e308db455f0d281ad7b006f
EvanSlyter <82098815+EvanSlyter@users.noreply.github.com>
2021-09-03 11:27:08 -0500

    Create Phone.java

---
+++ Unit2/2.1.9/Phone.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * The phone class represents a cellular phone.
+ * 
+ * Add the instance variables you think a phone would need
+ */ 
+
+public class Phone
+{
+
+    String number;
+    String brand;
+    String color;
+    String model;
+    // Attributes
+   
+    
+
+}
