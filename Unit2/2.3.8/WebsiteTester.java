e7a034bf7e21307ea138da5bc1c992936389bbdd
EvanSlyter <82098815+EvanSlyter@users.noreply.github.com>
2021-09-09 13:05:20 -0500

    Create WebsiteTester.java

---
+++ Unit2/2.3.8/WebsiteTester.java
@@ -0,0 +1,13 @@
+public class WebsiteTester
+{
+    public static void main(String[] args)
+    {
+        Website firstOne=new Website();
+        Website secondOne= new Website("evan'swebsite","net");
+        Website thirdOne= new Website("Evan'scoolerwebsite","org");
+        System.out.println(firstOne);
+        System.out.println(secondOne);
+        System.out.println(thirdOne);
+        
+    }
+}
