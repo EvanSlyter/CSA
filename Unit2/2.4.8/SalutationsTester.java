62768232eecc64daa3801020f4f764765c2cce3f
EvanSlyter <82098815+EvanSlyter@users.noreply.github.com>
2021-09-10 13:31:24 -0500

    Create SalutationsTester.java

---
+++ Unit2/2.4.8/SalutationsTester.java
@@ -0,0 +1,11 @@
+public class SalutationsTester
+{
+    public static void main(String[] args)
+    {
+        Salutations p1=new Salutations("Evan");
+        p1.addressLetter();
+        p1.signLetter();
+        p1.addressMemo();
+        p1.signMemo();
+    }
+}
