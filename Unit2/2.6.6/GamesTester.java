677d2c4653aba82ec92f472b921ea4433b6f440b
EvanSlyter <82098815+EvanSlyter@users.noreply.github.com>
2021-09-16 13:21:03 -0500

    Create GamesTester.java

---
+++ Unit2/2.6.6/GamesTester.java
@@ -0,0 +1,20 @@
+public class GamesTester
+{
+    public static void main(String[] args)
+    {
+        NumberGames game = new NumberGames(3);
+        
+        System.out.println(game.doubleNumber());
+        System.out.println(game.squareNumber());
+        System.out.println(game.doubleNumber());
+        System.out.println(game.getNumber());
+        
+        // Double the number again
+        // Print it out
+        
+        // Get the number and store the value
+        // Print it out to see that getNumber does
+        // not modify the number
+
+    }
+}
