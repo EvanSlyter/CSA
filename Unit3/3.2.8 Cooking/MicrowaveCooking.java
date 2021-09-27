241aaa037c9231b67905fea4642202b2aa3ea7c4
EvanSlyter <82098815+EvanSlyter@users.noreply.github.com>
2021-09-26 22:08:13 -0500

    Create MicrowaveCooking.java

---
+++ Unit3/3.2.8 Cooking/MicrowaveCooking.java
@@ -0,0 +1,21 @@
+public class MicrowaveCooking
+{
+    public static void main(String[] args)
+    {
+        int cookTime=(int)(Math.random()*61);
+        System.out.println("Microwaving for "+cookTime+" seconds");
+        if(cookTime<=20){
+         System.out.println("Perfect cooking time!");   
+        }
+        else{
+         System.out.println("Your roll will catch fire!");   
+        }
+       // Generate a random number of seconds
+       // between 0 and 60
+       
+       // Print the number of seconds
+       
+       // Use two if statements to print
+       // whether the roll is fine or will catch fire
+    }
+}
