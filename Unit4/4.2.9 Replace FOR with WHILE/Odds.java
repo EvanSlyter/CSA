a26961cb700402eae96c7b135b01fe9bbcea3f37
EvanSlyter <82098815+EvanSlyter@users.noreply.github.com>
2021-10-19 13:13:25 -0500

    Create Odds.java

---
+++ Unit4/4.2.9 Replace FOR with WHILE/Odds.java
@@ -0,0 +1,13 @@
+public class Odds
+{
+    public static void main(String[] args)
+    {
+        // Run this code first to see what is does. 
+        // Then replace the for loop with an equivalent while loop.
+      int  i=1;
+while(i<=10){
+    System.out.println(i);
+    i=i+2;
+}
+    }
+}
