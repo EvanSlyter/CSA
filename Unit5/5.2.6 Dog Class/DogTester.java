67b9c73f44ab360eb8c4d6070c1bd1dbd80b23c5
EvanSlyter <82098815+EvanSlyter@users.noreply.github.com>
2021-11-01 13:13:35 -0500

    Create DogTester.java

---
+++ Unit5/5.2.6 Dog Class/DogTester.java
@@ -0,0 +1,11 @@
+public class DogTester
+{
+    public static void main(String[] args)
+    {
+        Dog Karel=new Dog("Karel",7,15.6);
+        Dog Clover=new Dog("Clover",9);
+        System.out.println(Karel);
+        System.out.println(Clover);
+        // Start here!
+    }
+}
