41ac2b9d76446f3018dd3c1d78040b5298a31ad0
EvanSlyter <82098815+EvanSlyter@users.noreply.github.com>
2021-11-07 12:38:37 -0600

    Create CYOA.java

---
+++ Unit5/5.3.7 C.Y.O.A. Layout/CYOA.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CYOA
+{
+    public static void main(String[] args)
+    {
+        //first choice: Pizza or Burgers
+        
+            //user chooses Pizza
+                
+                //Second choice: Eat out or order delivery
+                    
+                    //user chooses eat out
+                        //Ending 1
+                    
+                    //user chooses order delivery
+                        //Ending 2
+                
+                //User chooses burgers
+                    
+                    //Second choice: Make your own or go to a restaurant 
+                    
+                        //user chooses make your own
+                            //Ending 3
+                            
+                        //user chooses go to a restaurant
+                            //Ending 4
+    }
+}
