95998215c9cf2ccbf413e44c630aceb69e53143d
EvanSlyter <82098815+EvanSlyter@users.noreply.github.com>
2021-12-01 12:58:27 -0600

    Create FirstArray.java

---
+++ Unit6/6.1.6 Our First Array/FirstArray.java
@@ -0,0 +1,25 @@
+public class FirstArray
+{
+    public static void main(String[] args)
+    {
+        String[] cityNames={"Las Vegas","Minsk","Sao Paulo"};
+        
+        int[] cityHighs={104,73,80};
+        
+        double[] cityRain={4.17,26.7,55};
+        
+        System.out.println(cityNames[0]+" has an average annual precipitation of "+cityRain[0]+" inches.");
+        System.out.println(cityNames[0]+" has an average annual high temp of "+cityHighs[0]+" degrees Fahrenheit.");
+        
+        System.out.println(cityNames[1]+" has an average annual precipitation of "+cityRain[1]+" inches.");
+        System.out.println(cityNames[1]+" has an average annual high temp of "+cityHighs[1]+" degrees Fahrenheit.");
+    
+        System.out.println(cityNames[2]+" has an average annual precipitation of "+cityRain[2]+" inches.");
+        System.out.println(cityNames[2]+" has an average annual high temp of "+cityHighs[2]+" degrees Fahrenheit.");
+      // Create the 3 arrays here
+      
+        
+      // Print all 3 arrays according to the output in the description
+    
+    }
+}
