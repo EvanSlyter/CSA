5f6f95c54f7c15fb34aaa4aa7c2dc7b49697a9af
EvanSlyter <82098815+EvanSlyter@users.noreply.github.com>
2022-01-24 13:16:27 -0600

    Create numbers.java

---
+++ Unit7/7.2.6 Get First Element/numbers.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Numbers
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        
+        System.out.println(numbers.get(0));
+        // Add 5 numbers to `numbers`
+        
+        // Print out the first element in `numbers`
+    }
+}
