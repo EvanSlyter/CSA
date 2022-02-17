2d0ce945b78895abde1fddb8a9aaaa60f538b04d
EvanSlyter <82098815+EvanSlyter@users.noreply.github.com>
2022-02-17 10:35:00 -0600

    Create TicTacToe.java

---
+++ Unit8/8.1.7 Tic Tac Toe Board/TicTacToe.java
@@ -0,0 +1,18 @@
+public class TicTacToe
+{
+    private String[][] board;
+    
+    public TicTacToe(){
+        String[][] board={
+        {"-","-","-"},
+        {"-","-","-"},
+        {"-","-","-"}
+        };
+        this.board=board;
+    }
+    
+    public String[][] getBoard(){
+     return board;   
+    }
+    
+}
