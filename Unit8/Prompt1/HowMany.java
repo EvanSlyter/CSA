34deff0a73621f20c8a89f80103c00f2670d1fd3
EvanSlyter <82098815+EvanSlyter@users.noreply.github.com>
2022-02-23 12:56:17 -0600

    Create HowMany.java

---
+++ Unit8/Prompt1/HowMany.java
@@ -0,0 +1,17 @@
+public class HowMany {
+    private static int count=0;
+    
+    public static void HowMany(int[][] arr,int target){
+     
+        for (int i=0;i<arr.length;i++){
+            
+            for(int k=0;k<arr[i].length;k++){
+                if (arr[i][k]==target){
+                    count++;   
+                }
+            }
+             
+         }
+         System.out.println("The " +target+ " count is :: "+count);
+    }
+}
